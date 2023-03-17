f0a61ff90acd0585b911e0aeaa8c20f0ab0f9082
jonnydavies1 <jonnydavies@gmail.com>
2023-03-17 18:00:22 +1300

    Implemented new features as requested

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const emojis = document.querySelectorAll(".emoji");
+const thankYouScreen = document.querySelector(".thank-you-screen");
 
 emojis.forEach(emoji => {
     emoji.addEventListener("click", (event) => {
@@ -6,5 +7,9 @@ emojis.forEach(emoji => {
         console.log(`Clicked emoji with ${points} points.`);
         
         // Send the points to your backend server or perform any desired action here
+        
+        // Hide the emoji sentiment app and show the thank you screen
+        document.querySelector(".emoji-sentiment-app").style.display = "none";
+        thankYouScreen.style.display = "block";
     });
 });
